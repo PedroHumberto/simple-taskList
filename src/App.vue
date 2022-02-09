9a3fce723046c243bc17052a73306382c0fc20c6
Pedro Humberto <pedrohumbertogc@gmail.com>
2022-02-08 22:04:11 -0300

    addtask component

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Task List</h1>
+    <add-task></add-task>
+
+  </div>
+
+</template>
+
+<script>
+import AddTask from './components/AddTask.vue'
+
+
+export default {
+  components:{
+    'add-task': AddTask
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/AddTask.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <input type="text" v-model="item">
+        <button @click="submitTask">Add Task</button>
+        <ul>
+            <li v-for="task in tasks" :key="task.id">
+                {{ task }}
+            </li>
+        </ul>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    
+    
+    data() {
+        return {
+            tasks: [],
+            item: ''
+        }
+    },
+    methods: {
+        submitTask(){
+            if(this.item != ''){
+                this.tasks.push(this.item)
+            }
+            this.item = ''
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
