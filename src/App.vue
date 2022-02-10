a5df2d4418864b7a2ce8640947656d8337467661
Pedro Humberto <pedrohumbertogc@gmail.com>
2022-02-09 21:05:16 -0300

    check and open, fetch

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <div>
-    <h1>Task List</h1>
-    <input type="text" v-model="item">
-    <button @click="submitTask">Add Task</button>
-    <add-task :tasks="tasks"></add-task>
+  <div id="task-app" class="container">
+    <div class="subcontainer">
 
+      <h1>Task List</h1>
+      <input type="text" v-model="item">
+      <div class="addbutton">
+
+        <button @click="submitTask">Add Task</button>
+      </div>
+      <add-task :tasks="tasks"></add-task>
+    </div>
   </div>
 
 </template>
@@ -12,28 +17,64 @@
 <script>
 import AddTask from './components/AddTask.vue'
 
-
 export default {
   components:{
     'add-task': AddTask
   },
   data() {
     return {
+      id: 5,
       item: '',
-      tasks: [],
+      tasks: []
     }
   },
-   methods: {
-        submitTask(){
-            if(this.item != ''){
-                this.tasks.push(this.item)
-            }
-            this.item = ''
-        }
-    },
+  created() {
+    fetch('../data/taskList.json')
+    .then(res => res.json())
+    .then(data => this.tasks = data)
+  },
+  methods: {
+      submitTask(){
+         if(this.item != ''){
+            this.tasks.push({id: this.id++, item: this.item, status: false})
+          }
+          this.item = ''
+      }
+  },
 }
 </script>
 
 <style>
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  .container{
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-items: center;
+    margin: 15px auto;
+    height: 500px;
+  }
+  .subcontainer{
+    border: 3px dashed green;
+    border-radius: 10px;
+    margin: 10px auto;
+    width: 400px;
+  }
+  h1, input{
+    display: flex;
+    justify-content: center;
+
+    margin: 5px auto;
+  }
+  .addbutton{
+    display: flex;
+    justify-content: center;
+    margin-bottom: 10px;
+  }
+
 
 </style>
--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -1,12 +1,21 @@
 <template>
     <div>
-        
-        <ul>
-            <li v-for="task in tasks" :key="task.id">
-                {{ task }}
-            </li>
-        </ul>
-        
+        <div class="card">
+            <ul>
+                <li v-for="task in tasks" :key="task.id">
+                    <div v-if="task.status == false">
+                        <button @click="check(task)">Done</button>
+                        {{ task.item }}
+                        
+                    </div>
+                    <div v-else style="text-decoration: line-through">
+                        <button @click="open(task)">Open</button>
+                        {{ task.item }}
+                        
+                    </div>
+                </li>
+            </ul>       
+        </div>
     </div>
 </template>
 
@@ -15,20 +24,34 @@ export default {
     props: {
         tasks:{
             required: true,
-            type: Array,
-            default: []
+            type: Array | Object,
         }
-    }, 
-    
-    data() {
-        return {
+    },     
+    methods: {
+        check(task){
+            task.status = true
+        },
+        open(task){
+            task.status = false
         }
-    },
-   
-
+    }
 }
 </script>
 
-<style>
+<style scoped>
+    .card{
+        border: 2px solid rgb(130, 190, 247);
+        border-radius: 10px;
+        background-color: rgb(253, 239, 221);
+        margin: 10px;
+    }
+    li{
+        display: flex;
+        margin: 4px auto;
+    }
+    button{
+        margin-left: 30px;
+    }
+</style>>
 
 </style>
